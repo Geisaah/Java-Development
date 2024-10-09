98fa7e33cf0ffc62dde7e78b0a4add3cbdeb88be
geisaah <geisaahfreitas@gmail.com>
2024-10-09 17:28:08 -0300

    Operators

---
+++ src/marathon/javadevelopment/introduction/Operators.java
@@ -0,0 +1,13 @@
+package marathon.javadevelopment.introduction;
+
+public class Operators {
+    public static void main(String[] args) {
+        // + - / *
+        int number01 = 10;
+        int number02 = 20;
+        int result = number01 * number02;
+        System.out.println(result);
+        System.out.println(number01+number02);
+        System.out.println(number02+number01+ " value " +number02+number01);
+    }
+}
