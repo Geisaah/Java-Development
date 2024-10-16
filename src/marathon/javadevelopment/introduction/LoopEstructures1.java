ab4508e2231a5af6140521c1f76990d12d0204bb
geisaah <geisaahfreitas@gmail.com>
2024-10-16 17:42:52 -0300

    Loop Estructures1

---
+++ src/marathon/javadevelopment/introduction/LoopEstructures1.java
@@ -0,0 +1,12 @@
+package marathon.javadevelopment.introduction;
+
+public class LoopEstructures1 {
+    public static void main(String[] args) {
+        //print all even number from 0 as 100000
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
