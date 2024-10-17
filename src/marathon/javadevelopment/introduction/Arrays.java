2b60b16be1faa8cd58d053e308afb5dd22d651a1
geisaah <geisaahfreitas@gmail.com>
2024-10-17 16:33:22 -0300

    Arrays

---
+++ src/marathon/javadevelopment/introduction/Arrays.java
@@ -0,0 +1,14 @@
+package marathon.javadevelopment.introduction;
+
+public class Arrays {
+    public static void main(String[] args) {
+       // int[] age = null;
+        int[] age = new int[3];
+        age[0] = 21;
+        age[1] = 15;
+        age[2] = 11;
+        System.out.println(age[0]);
+        System.out.println(age[1]);
+        System.out.println(age[2]);
+    }
+}
