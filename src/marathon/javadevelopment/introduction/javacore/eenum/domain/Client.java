bd0df883449ad8dc9939156f3a8653435a0115da
geisaah <geisaahfreitas@gmail.com>
2024-10-30 19:29:09 -0300

    Object-oriented Enum2 +test

--- src/marathon/javadevelopment/introduction/javacore/eenum/domain/Client.java
+++ src/marathon/javadevelopment/introduction/javacore/eenum/domain/Client.java
@@ -2,24 +2,18 @@ package marathon.javadevelopment.introduction.javacore.eenum.domain;
 
 public class Client {
     private String name;
-    private String type;
-    public static final String PERSON_NATURAL = "PERSON_NATURAL";
-    public static final String LEGAL_PERSON = "LEGAL_PERSON";
+    private ClientType clientType;
 
-
-    public Client(String name, String type) {
-        if (!type.equals(PERSON_NATURAL) == false && !type.equals(LEGAL_PERSON) == false){
-            return;
-        }
+    public Client(String name, ClientType clientType) {
         this.name = name;
-        this.type = type;
+        this.clientType = clientType;
     }
 
     @Override
     public String toString() {
         return "Client{" +
                 "name='" + name + '\'' +
-                ", type='" + type + '\'' +
+                ", clientType=" + clientType +
                 '}';
     }
 
@@ -31,11 +25,16 @@ public class Client {
         this.name = name;
     }
 
-    public String getType() {
-        return type;
+    public ClientType getClientType() {
+        return clientType;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setClientType(ClientType clientType) {
+        this.clientType = clientType;
     }
+    // private String type;
+   // public static final String PERSON_NATURAL = "PERSON_NATURAL";
+   // public static final String LEGAL_PERSON = "LEGAL_PERSON";
+
+
 }
--- src/marathon/javadevelopment/introduction/javacore/eenum/test/ClientTest.java
+++ src/marathon/javadevelopment/introduction/javacore/eenum/test/ClientTest.java
@@ -1,13 +1,14 @@
 package marathon.javadevelopment.introduction.javacore.eenum.test;
 
 import marathon.javadevelopment.introduction.javacore.eenum.domain.Client;
+import marathon.javadevelopment.introduction.javacore.eenum.domain.ClientType;
 
 public class ClientTest {
     public static void main(String[] args) {
-        Client client = new Client("Weverton", "Natural Person");
-        Client client1 = new Client("Weverton", "Legal Person");
-        Client client2 = new Client("Weverton", "NATURAL PERSON");
-        Client client3 = new Client("Weverton", "LEGAL PERSON");
+        Client client = new Client("Weverton", ClientType.LEGAL_PERSON);
+        Client client1 = new Client("Weverton", ClientType.LEGAL_PERSON);
+        Client client2 = new Client("Weverton", ClientType.PERSON_NATURAL);
+        Client client3 = new Client("Weverton", ClientType.PERSON_NATURAL);
         System.out.println(client);
         System.out.println(client1);
         System.out.println(client2);
