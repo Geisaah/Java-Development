d11ac5e50ed4f3108f6409b1429e367d71ea45f8
geisaah <geisaahfreitas@gmail.com>
2024-11-07 19:22:53 -0300

    Calendar +Test

---
+++ src/marathon/javadevelopment/introduction/javacore/dates/test/CalendarTest.java
@@ -0,0 +1,23 @@
+package marathon.javadevelopment.introduction.javacore.dates.test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalendarTest {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+       if(  c.getFirstDayOfWeek() == Calendar.SUNDAY){
+           System.out.println("Sunday is the first day of the week");
+       }
+        System.out.println(c.get(Calendar.DAY_OF_WEEK));
+        System.out.println(c.get(Calendar.DAY_OF_MONTH));
+        System.out.println(c.get(Calendar.DAY_OF_YEAR));
+        System.out.println(c.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+
+        c.add(Calendar.DAY_OF_MONTH,2);
+        c.add(Calendar.HOUR,2);
+
+        Date date = c.getTime();
+        System.out.println(c);
+    }
+}
